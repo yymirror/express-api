bcc0abfcce1a067815a3f9815ecd3f3d7dd77904
Marcel (Bourne Again) <yyscript@proton.me>
2023-02-15 19:49:15 +0100

    Initial

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const app = express();
+
+
+function conv(data, from) {
+    let to = from === "utf-8" ? "base64" : "utf-8";
+    let buffer = Buffer.from(data, from);
+
+    return buffer.toString(to);
+}
+
+app.get("/", async function (req, res) {
+    let data = req.query.data || "Hello World";
+    let type = req.query.type || "utf-8";
+
+    let value = conv(data, type);
+
+    res.send(value);
+});
+
+app.listen(process.env.PORT || 3000);
